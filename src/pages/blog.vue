76193e9800e9dfa88a4eaccc3aa47c1dd6775927
wangyulin <wang271934@163.com>
2022-09-23 19:08:46 +0800

    feat: npm codepen

--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -1,14 +1,24 @@
 <template>
-    <div class="blog-container">
-        <div class="blog-content" v-if="!preview"></div>
-        <div class="blog-user">
-            <img class="user-header" src="@/assets/images/blog/header.jpeg" alt="">
-            <p class="info-text user-introduce">Hello,I'm ulin Wang and that is my cool face.</p>
-            <p class="elsewhere">Elsewhere</p>
-            <div class="elsewhere-box" @click="toElsewhere">
-                <div class="elsewhere-list" v-for="item in elsewhereList" :key="item.label" :data-url="item.url">
-                    <img class="elsewhere-icon" :src="required(item.src)" :data-url="item.url" alt="">
-                    <span class="elsewhere-span" :data-url="item.url">{{ item.label }}</span>
+    <div class="vs-padding">
+        <div class="blog-container">
+            <div class="blog-content" v-if="!preview">
+                <div v-for="(item, index) in blogList" :key="index" class="blog-list-container">
+                    <p class="blog-title page-title">{{ item.title }}</p>
+                    <div class="main-box">
+                        <p class="blog-time info-text">Posted {{ item.time }}</p>
+                        <p class="blog-preview info-text">{{ item.preview }}...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="blog-user">
+                <img class="user-header" src="@/assets/images/blog/header.jpeg" alt="">
+                <p class="info-text user-introduce">Hello,I'm ulin Wang and that is my cool face.</p>
+                <p class="elsewhere">Elsewhere</p>
+                <div class="elsewhere-box" @click="toElsewhere">
+                    <div class="elsewhere-list" v-for="item in elsewhereList" :key="item.label" :data-url="item.url">
+                        <img class="elsewhere-icon" :src="required(item.src)" :data-url="item.url" alt="">
+                        <span class="elsewhere-span" :data-url="item.url">{{ item.label }}</span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -16,17 +26,43 @@
 </template>
 
 <script lang="ts" setup>
+export interface IBlogItem {
+    title: string,
+    time: string,
+    preview: string,
+}
+
+export interface IElsewhereItem {
+    src: string,
+    label: string,
+    url: string,
+}
+
 const prop = defineProps<{
     preview?: boolean
 }>()
+
 const preview = prop.preview || false
+
 const required = (name: string) => {
   return new URL(`/src/assets/images/blog/${name}.png`, import.meta.url).href
 }
-const elsewhereList = [
+
+const elsewhereList: IElsewhereItem[] = [
     { src: 'github', label: 'github', url: 'https://github.com/webwangyl', },
-    { src: 'Sina-Weibo', label: 'Sina-Weibo', url: '' }
+    { src: 'Sina-Weibo', label: 'Sina-Weibo', url: '' },
+    { src: 'npm', label: 'npm', url: 'https://www.npmjs.com/~wangyulin' },
+    { src: 'codepen', label: 'codepen', url: 'https://codepen.io/webwangyl' }
 ]
+
+const blogList: IBlogItem[] = [
+    { title: 'asdasdas把咖啡馆代理商会计分录收到', time: '2020:2!@', preview: '结案率看帅哥克拉几十个捣乱时光倒流科技感控制臂嘎良好的' },
+    { title: 'asdasdas把咖啡馆代理商会计分录收到', time: '2020:2!@', preview: '结案率看帅哥克拉几十个捣乱时光倒流科技感控制臂嘎良好的' },
+    { title: 'asdasdas把咖啡馆代理商会计分录收到', time: '2020:2!@', preview: '结案率看帅哥克拉几十个捣乱时光倒流科技感控制臂嘎良好的' },
+    { title: 'asdasdas把咖啡馆代理商会计分录收到', time: '2020:2!@', preview: '结案率看帅哥克拉几十个捣乱时光倒流科技感控制臂嘎良好的' },
+    { title: 'asdasdas把咖啡馆代理商会计分录收到', time: '2020:2!@', preview: '结案率看帅哥克拉几十个捣乱时光倒流科技感控制臂嘎良好的' },
+]
+
 const toElsewhere = (e: MouseEvent) => {
     const url = (e.target as HTMLElement).getAttribute('data-url')
     if (url) {
@@ -36,6 +72,9 @@ const toElsewhere = (e: MouseEvent) => {
 </script>
 
 <style lang="scss" scoped>
+.vs-padding {
+    padding-bottom: 20px;
+}
 .blog-container {
     width: calc(100% - 300px);
     min-height: calc(100vh - 240px);
@@ -46,6 +85,28 @@ const toElsewhere = (e: MouseEvent) => {
     padding: 20px 0;
     .blog-content {
         flex: 1;
+        .blog-list-container {
+            .blog-title {
+                background-color: #ff931d;
+                padding-left: 20px;
+                border-radius: 32px 0 0 32px;
+                white-space: pre-wrap;
+                margin-bottom: 0;
+                transform: translateX(-32px);
+            }
+            .main-box {
+                padding-right: 32px;
+                .blog-time {
+                    background-color: #ffc244;
+                    color: #000;
+                    line-height: 32px;
+                }
+                .blog-preview {
+                    padding: 20px 24px;
+                    color: var(--color-inside);
+                }
+            }
+        }
     }
     .blog-user {
         width: 300px;
